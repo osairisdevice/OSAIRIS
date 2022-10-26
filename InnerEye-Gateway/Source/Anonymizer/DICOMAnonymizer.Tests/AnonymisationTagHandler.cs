930f8fcc5f30a4f5eb2154f92c554bac8b8b7f0c
osairisdevice <116715049+osairisdevice@users.noreply.github.com>
2022-10-26 10:24:49 +0100

    Add files via upload

---
+++ InnerEye-Gateway/Source/Anonymizer/DICOMAnonymizer.Tests/AnonymisationTagHandler.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+namespace DICOMAnonymizer.Tests
+{
+    using System.Collections.Generic;
+    using System.Text.RegularExpressions;
+    using Dicom;
+    using DICOMAnonymizer;
+    using AnonFunc = System.Func<Dicom.DicomDataset, System.Collections.Generic.List<TagOrIndex>, Dicom.DicomItem, Dicom.DicomItem>;
+
+    internal class AnonymisationTagHandler : ITagHandler
+    {
+
+        /// <summary>
+        /// The anonymisation protocol.
+        /// </summary>
+        private readonly Dictionary<DicomTag, AnonFunc> _anonymisationProtocol = new Dictionary<DicomTag, AnonFunc>
+        {
+            { DicomTag.PatientID, (ds,tagOrIndexes, dicomItem)=> dicomItem },
+            { DicomTag.Modality, (ds,tagOrIndexes, dicomItem)=> dicomItem },
+            { DicomTag.SOPClassUID, (ds,tagOrIndexes, dicomItem)=> new DicomUniqueIdentifier(DicomTag.SOPClassUID,DicomUIDGenerator.GenerateDerivedFromUUID()) },
+            { DicomTag.SOPInstanceUID, (ds,tagOrIndexes, dicomItem)=> new DicomUniqueIdentifier(DicomTag.SOPInstanceUID,DicomUIDGenerator.GenerateDerivedFromUUID()) },
+        };
+
+        // TODO refactor into abstract class
+        public Dictionary<string, string> GetConfiguration() => null;
+
+        // TODO refactor into abstract class
+        public Dictionary<Regex, AnonFunc> GetRegexFuncs() => null;
+
+        // TODO refactor into abstract class
+        public Dictionary<DicomTag, AnonFunc> GetTagFuncs() => _anonymisationProtocol;
+
+        // TODO refactor into abstract class
+        public void NextDataset()
+        {
+        }
+
+        public void Postprocess(DicomDataset newds)
+        {
+            // NOOP
+        }
+    }
+}
---
+++ InnerEye-Gateway/Source/Anonymizer/DICOMAnonymizer/TagOrIndex.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+namespace DICOMAnonymizer
+{
+    using System.Globalization;
+    using Dicom;
+
+    public class TagOrIndex
+    {
+        public bool IsTag { get; }
+        public DicomTag Tag { get; set; }
+        public int Index { get; set; } = -1;
+
+        public TagOrIndex(DicomTag tag)
+        {
+            Tag = tag;
+            IsTag = true;
+        }
+
+        public TagOrIndex(int index)
+        {
+            Index = index;
+            IsTag = false;
+        }
+
+        public override string ToString()
+        {
+            if (IsTag)
+            {
+                return Tag.ToString();
+            }
+            return Index.ToString(CultureInfo.InvariantCulture);
+        }
+    }
+}
